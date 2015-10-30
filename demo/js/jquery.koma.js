f473a67f95a537e75c75c088a23d0676c2c9906f
mb_kondou <kondou@maboroshi.biz>
2015-10-30 13:36:24 +0900

    support IE8

--- demo/js/jquery.koma.js
+++ demo/js/jquery.koma.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
-
 (function () {
 	var requestAnimationFrame = (function () {
 		return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
@@ -17,173 +13,156 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 		return now && now.call(performance) || new Date().getTime();
 	};
 
+	var loop = true;
+
 	/**
   * Creates a new Koma.
   * @class
   */
+	function Koma($el, op) {
+		this.option = $.extend({
+			'fps': 20,
+			'steps': [],
+			'itemEl': '.koma-items',
+			'restartEl': '.koma-restart',
+			'stopEl': '.koma-stop'
+		}, op);
+		this.$el = $el;
+		this.$img = this.$el.find(this.option.itemEl + ' img');
+		this.$item = this.$el.find(this.option.itemEl);
+		this.$restart = this.$el.find(this.option.restartEl);
+		this.$stop = this.$el.find(this.option.stopEl);
+		this.imgLen = this.$img.length;
+		this.startTime = getTime();
+		this.animationFlag = true;
+		this.isSteps = this.option.steps.length > 0 ? true : false;
+		this.setup();
+	}
 
-	var Koma = (function () {
-		/**
-   * Initialize
-   * @param $el {element} peaent element
-   * @param op {object} plugin options
-   * @return undefined
-   */
+	/**
+  * Setup
+  * @return undefined
+  */
+	Koma.prototype.setup = function () {
+		var _this = this;
 
-		function Koma($el, op) {
-			_classCallCheck(this, Koma);
+		this.$img.slice(1).hide();
+		this.imgLoad().then(function () {
+			_this.animation();
+			_this.eventify();
+			if (_this.isSteps) _this.imgLoadSteps();
+		}).fail(function () {
+			console.log('image load error');
+		});
+	};
 
-			this.option = $.extend({
-				'fps': 20,
-				'steps': [],
-				'itemEl': '.koma-items',
-				'restartEl': '.koma-restart',
-				'stopEl': '.koma-stop'
-			}, op);
-			this.$el = $el;
-			this.$img = this.$el.find(this.option.itemEl + ' img');
-			this.$item = this.$el.find(this.option.itemEl);
-			this.$restart = this.$el.find(this.option.restartEl);
-			this.$stop = this.$el.find(this.option.stopEl);
-			this.imgLen = this.$img.length;
-			this.startTime = getTime();
-			this.animationFlag = true;
-			this.isSteps = this.option.steps.length > 0 ? true : false;
-			this.setup();
+	/**
+  * イベント設定
+  * @return undefined
+  */
+	Koma.prototype.eventify = function () {
+		var _this2 = this;
+
+		var stopEvent = new $.Event('koma_stop');
+		var restartEvent = new $.Event('koma_restart');
+
+		this.$stop.on('click', function () {
+			if (!window.cancelAnimationFrame) {
+				loop = false;
+			} else {
+				window.cancelAnimationFrame(_this2.requestId);
+			}
+			_this2.$el.trigger(stopEvent, { $el: _this2.$el });
+		});
+		this.$restart.on('click', function () {
+			loop = true;
+			_this2.requestId = requestAnimationFrame($.proxy(_this2.animation, _this2));
+			_this2.$el.trigger(restartEvent, { $el: _this2.$el });
+		});
+	};
+
+	/**
+  * 画像読み込み
+  * @return promise
+  */
+	Koma.prototype.imgLoad = function () {
+		var _this3 = this;
+
+		var startNum = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
+		var endNum = arguments.length <= 1 || arguments[1] === undefined ? this.isSteps ? this.option.steps[0] : this.imgLen : arguments[1];
+
+		var promises = [];
+		var retDefer = $.Deferred();
+		this.imgLen = this.isSteps ? this.option.steps[0] : this.imgLen;
+
+		var _loop = function (i) {
+			var img = new Image();
+			var defer = $.Deferred();
+			img.onload = function () {
+				defer.resolve();
+			};
+			img.error = function () {
+				defer.reject();
+			};
+			img.src = _this3.$img.eq(i).attr('src');
+			promises.push(defer);
+		};
+
+		for (var i = startNum; i < endNum; i++) {
+			_loop(i);
 		}
 
-		/**
-   * Setup
-   * @return undefined
-   */
+		$.when.apply(null, promises).done(function () {
+			retDefer.resolve();
+		}).fail(function () {
+			retDefer.reject();
+		});
 
-		_createClass(Koma, [{
-			key: 'setup',
-			value: function setup() {
-				var _this = this;
+		return retDefer.promise();
+	};
 
-				this.$img.slice(1).hide();
-				this.imgLoad().then(function () {
-					_this.animation();
-					_this.eventify();
-					if (_this.isSteps) _this.imgLoadSteps();
-				}).fail(function () {
-					console.log('image load error');
-				});
-			}
+	/**
+  * 画像分割読み込み
+  * @return undefind
+  */
+	Koma.prototype.imgLoadSteps = function () {
+		var _this4 = this;
 
-			/**
-    * イベント設定
-    * @return undefined
-    */
-		}, {
-			key: 'eventify',
-			value: function eventify() {
-				var _this2 = this;
+		var steps = this.option.steps;
+		var stepArr = steps.length;
+		var promise = $.Deferred().resolve();
+		var endFrame = 0;
+		var startFrame = steps[0];
 
-				var stopEvent = new $.Event('koma_stop');
-				var restartEvent = new $.Event('koma_restart');
-				this.$stop.on('click', function () {
-					window.cancelAnimationFrame(_this2.requestId);
-					_this2.$el.trigger(stopEvent, { $el: _this2.$el });
-				});
-				this.$restart.on('click', function () {
-					_this2.requestId = requestAnimationFrame(_this2.animation.bind(_this2));
-					_this2.$el.trigger(restartEvent, { $el: _this2.$el });
-				});
-			}
+		var _loop2 = function (i) {
+			promise = promise.then(function () {
+				endFrame = startFrame + steps[i];
+				return _this4.imgLoad(startFrame, endFrame);
+			}).then(function () {
+				startFrame = endFrame;
+				_this4.imgLen = endFrame;
+			}).fail(function () {
+				console.log('image load error');
+			});
+		};
 
-			/**
-    * 画像読み込み
-    * @return promise
-    */
-		}, {
-			key: 'imgLoad',
-			value: function imgLoad() {
-				var _this3 = this;
+		for (var i = 1; i < stepArr; i++) {
+			_loop2(i);
+		}
+	};
 
-				var startNum = arguments.length <= 0 || arguments[0] === undefined ? 0 : arguments[0];
-				var endNum = arguments.length <= 1 || arguments[1] === undefined ? this.isSteps ? this.option.steps[0] : this.imgLen : arguments[1];
-
-				var promises = [];
-				var retDefer = $.Deferred();
-				this.imgLen = this.isSteps ? this.option.steps[0] : this.imgLen;
-
-				var _loop = function (i) {
-					var img = new Image();
-					var defer = $.Deferred();
-					img.src = _this3.$img.eq(i).attr('src');
-					img.onload = function () {
-						defer.resolve();
-					};
-					img.error = function () {
-						defer.reject();
-					};
-					promises.push(defer);
-				};
-
-				for (var i = startNum; i < endNum; i++) {
-					_loop(i);
-				}
-
-				$.when.apply(null, promises).done(function () {
-					retDefer.resolve();
-				}).fail(function () {
-					retDefer.reject();
-				});
-
-				return retDefer.promise();
-			}
-
-			/**
-    * 画像分割読み込み
-    * @return undefind
-    */
-		}, {
-			key: 'imgLoadSteps',
-			value: function imgLoadSteps() {
-				var _this4 = this;
-
-				var steps = this.option.steps;
-				var stepArr = steps.length;
-				var promise = $.Deferred().resolve();
-				var endFrame = 0;
-				var startFrame = steps[0];
-
-				var _loop2 = function (i) {
-					promise = promise.then(function () {
-						endFrame = startFrame + steps[i];
-						return _this4.imgLoad(startFrame, endFrame);
-					}).then(function () {
-						startFrame = endFrame;
-						_this4.imgLen = endFrame;
-					}).fail(function () {
-						console.log('image load error');
-					});
-				};
-
-				for (var i = 1; i < stepArr; i++) {
-					_loop2(i);
-				}
-			}
-
-			/**
-    * コマ送りアニメーション
-    * @return promise
-    */
-		}, {
-			key: 'animation',
-			value: function animation() {
-				this.requestId = requestAnimationFrame(this.animation.bind(this));
-				var lastTime = getTime();
-				var frame = Math.floor((lastTime - this.startTime) / (1000.0 / this.option.fps) % this.imgLen);
-				if (frame === 0) frame = 1;
-				this.$img.hide().slice(frame - 1, frame).show();
-			}
-		}]);
-
-		return Koma;
-	})();
+	/**
+  * コマ送りアニメーション
+  * @return promise
+  */
+	Koma.prototype.animation = function () {
+		if (!loop) return;
+		this.requestId = requestAnimationFrame($.proxy(this.animation, this));
+		var lastTime = getTime();
+		var frame = Math.floor((lastTime - this.startTime) / (1000.0 / this.option.fps) % this.imgLen);
+		if (frame === 0) frame = 1;
+		this.$img.hide().slice(frame - 1, frame).show();
+	};
 
 	$.fn.koma = function (options) {
 		var _this5 = this;
--- src/jquery.koma.js
+++ src/jquery.koma.js
@@ -1,6 +1,5 @@
-
 () => {
-	var requestAnimationFrame = ( function(){
+	let requestAnimationFrame = ( function(){
 		return  window.requestAnimationFrame       || 
 				window.webkitRequestAnimationFrame || 
 				window.mozRequestAnimationFrame    || 
@@ -11,148 +10,149 @@
 				};
 	} )();
 
-	var now = window.performance && (
+	let now = window.performance && (
 		performance.now || 
 		performance.mozNow || 
 		performance.msNow || 
 		performance.oNow || 
 		performance.webkitNow );
 
-	var getTime = function() {
+	let getTime = function() {
 		return ( now && now.call( performance ) ) || ( new Date().getTime() );
 	};
+
+	let loop = true;
 
 	/**
 	 * Creates a new Koma.
 	 * @class
 	 */
-	class Koma {
-		/**
-		 * Initialize
-		 * @param $el {element} peaent element
-		 * @param op {object} plugin options
-		 * @return undefined
-		 */
-		constructor($el, op) {
-			this.option = $.extend({
-				'fps'       : 20,
-				'steps'     : [],
-				'itemEl'    : '.koma-items',
-				'restartEl' : '.koma-restart',
-				'stopEl'    : '.koma-stop'
-			}, op);
-			this.$el           = $el;
-			this.$img          = this.$el.find(this.option.itemEl + ' img');
-			this.$item         = this.$el.find(this.option.itemEl);
-			this.$restart      = this.$el.find(this.option.restartEl);
-			this.$stop         = this.$el.find(this.option.stopEl);
-			this.imgLen        = this.$img.length;
-			this.startTime     = getTime();
-			this.animationFlag = true;
-			this.isSteps       = this.option.steps.length > 0 ? true : false;
-			this.setup();
-		}
-
-		/**
-		 * Setup
-		 * @return undefined
-		 */
-		setup() {
-			this.$img.slice(1).hide();
-			this.imgLoad()
-				.then(() => {
-					this.animation();
-					this.eventify();
-					if(this.isSteps) this.imgLoadSteps();
-				}).fail(() => {
-					console.log('image load error'); 
-				});
-		}
-
-		/**
-		 * イベント設定
-		 * @return undefined
-		 */
-		eventify(){
-			let stopEvent    = new $.Event('koma_stop');
-			let restartEvent = new $.Event('koma_restart');
-			this.$stop.on('click', () => {
-				window.cancelAnimationFrame( this.requestId );
-				this.$el.trigger( stopEvent, {$el: this.$el} );
-			});
-			this.$restart.on('click', () => {
-				this.requestId = requestAnimationFrame( this.animation.bind(this) );
-				this.$el.trigger( restartEvent, {$el: this.$el} );
-			});
-		}
-
-		/**
-		 * 画像読み込み
-		 * @return promise
-		 */
-		imgLoad(startNum = 0, endNum = this.isSteps ? this.option.steps[0] : this.imgLen) {
-			let promises = [];
-			let retDefer = $.Deferred();
-			this.imgLen = this.isSteps ? this.option.steps[0] : this.imgLen;
-
-			for(let i = startNum;i < endNum;i++){
-				let img = new Image();
-				let defer = $.Deferred();
-				img.src = this.$img.eq(i).attr('src');
-				img.onload = function () {
-					defer.resolve();
-				};
-				img.error = function () {
-					defer.reject();
-				};
-				promises.push(defer);
-			}
-
-			$.when.apply(null, promises).done(() => {
-				retDefer.resolve();
-			}).fail(() => {
-				retDefer.reject();
-			});
-
-			return retDefer.promise();
-		}
-
-		/**
-		 * 画像分割読み込み
-		 * @return undefind
-		 */
-		imgLoadSteps() {
-			let steps      = this.option.steps;
-			let stepArr    = steps.length;
-			let promise    = $.Deferred().resolve();
-			let endFrame   = 0;
-			let startFrame = steps[0];
-
-			for(let i = 1;i < stepArr;i++){
-				promise = promise.then(() => {
-					endFrame = startFrame + steps[i];
-					return this.imgLoad(startFrame, endFrame);
-				}).then(() => {
-					startFrame  = endFrame;
-					this.imgLen = endFrame;
-				}).fail(() => {
-					console.log('image load error'); 
-				});
-			}
-		}
-
-		/**
-		 * コマ送りアニメーション
-		 * @return promise
-		 */
-		animation() {
-			this.requestId = requestAnimationFrame( this.animation.bind(this) );
-			let lastTime   = getTime();
-			let frame      = Math.floor( ( lastTime - this.startTime ) / ( 1000.0 / this.option.fps ) % this.imgLen );
-			if(frame === 0) frame = 1;
-			this.$img.hide().slice( (frame-1), frame ).show();
-		}
+	function Koma($el, op){
+		this.option = $.extend({
+			'fps'       : 20,
+			'steps'     : [],
+			'itemEl'    : '.koma-items',
+			'restartEl' : '.koma-restart',
+			'stopEl'    : '.koma-stop'
+		}, op);
+		this.$el           = $el;
+		this.$img          = this.$el.find(this.option.itemEl + ' img');
+		this.$item         = this.$el.find(this.option.itemEl);
+		this.$restart      = this.$el.find(this.option.restartEl);
+		this.$stop         = this.$el.find(this.option.stopEl);
+		this.imgLen        = this.$img.length;
+		this.startTime     = getTime();
+		this.animationFlag = true;
+		this.isSteps       = this.option.steps.length > 0 ? true : false;
+		this.setup();
 	}
+
+	/**
+	 * Setup
+	 * @return undefined
+	 */
+	Koma.prototype.setup = function(){
+		this.$img.slice(1).hide();
+		this.imgLoad()
+			.then(() => {
+				this.animation();
+				this.eventify();
+				if(this.isSteps) this.imgLoadSteps();
+			}).fail(() => {
+				console.log('image load error'); 
+			});
+	};
+
+	/**
+	 * イベント設定
+	 * @return undefined
+	 */
+	Koma.prototype.eventify = function(){
+		let stopEvent    = new $.Event('koma_stop');
+		let restartEvent = new $.Event('koma_restart');
+
+		this.$stop.on('click', () => {
+			if(!window.cancelAnimationFrame){
+				loop = false;
+			}else{
+				window.cancelAnimationFrame( this.requestId );
+			}
+			this.$el.trigger( stopEvent, {$el: this.$el} );
+		});
+		this.$restart.on('click', () => {
+			loop = true;
+			this.requestId = requestAnimationFrame( $.proxy(this.animation,this) );
+			this.$el.trigger( restartEvent, {$el: this.$el} );
+		});
+	};
+
+	/**
+	 * 画像読み込み
+	 * @return promise
+	 */
+	Koma.prototype.imgLoad = function(startNum = 0, endNum = this.isSteps ? this.option.steps[0] : this.imgLen) {
+		let promises = [];
+		let retDefer = $.Deferred();
+		this.imgLen = this.isSteps ? this.option.steps[0] : this.imgLen;
+
+		for(let i = startNum;i < endNum;i++){
+			let img = new Image();
+			let defer = $.Deferred();
+			img.onload = function () {
+				defer.resolve();
+			};
+			img.error = function () {
+				defer.reject();
+			};
+			img.src = this.$img.eq(i).attr('src');
+			promises.push(defer);
+		}
+
+		$.when.apply(null, promises).done(() => {
+			retDefer.resolve();
+		}).fail(() => {
+			retDefer.reject();
+		});
+
+		return retDefer.promise();
+	};
+
+	/**
+	 * 画像分割読み込み
+	 * @return undefind
+	 */
+	Koma.prototype.imgLoadSteps = function() {
+		let steps      = this.option.steps;
+		let stepArr    = steps.length;
+		let promise    = $.Deferred().resolve();
+		let endFrame   = 0;
+		let startFrame = steps[0];
+
+		for(let i = 1;i < stepArr;i++){
+			promise = promise.then(() => {
+				endFrame = startFrame + steps[i];
+				return this.imgLoad(startFrame, endFrame);
+			}).then(() => {
+				startFrame  = endFrame;
+				this.imgLen = endFrame;
+			}).fail(() => {
+				console.log('image load error'); 
+			});
+		}
+	};
+
+	/**
+	 * コマ送りアニメーション
+	 * @return promise
+	 */
+	Koma.prototype.animation = function() {
+		if(!loop) return;
+		this.requestId = requestAnimationFrame( $.proxy(this.animation,this) );
+		let lastTime   = getTime();
+		let frame      = Math.floor( ( lastTime - this.startTime ) / ( 1000.0 / this.option.fps ) % this.imgLen );
+		if(frame === 0) frame = 1;
+		this.$img.hide().slice( (frame-1), frame ).show();
+	};
 
 	$.fn.koma = function(options) {
 		return this.each(() => {
